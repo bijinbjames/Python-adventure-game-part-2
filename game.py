64a715726382bb849a60e001132faca322004279
bijinbjames <bijinbjames@gmail.com>
2023-01-02 01:14:15 +0530

    next page added

--- game.py
+++ game.py
@@ -8,24 +8,57 @@ class AdventureGame(tk.Tk):
         self.geometry("700x600")
 
         # Add a welcome label
-        welcome_label = tk.Label(self, text="Welcome! Let's go on an adventure.", font=("Helvetica", 16))
-        welcome_label.pack(side="top", fill="x", pady=10)
+        self.welcome_label = tk.Label(self, text="Welcome! Let's go on an adventure.", font=("Helvetica", 16))
+        self.welcome_label.pack(side="top", fill="x", pady=10)
 
         # Add an image
-        image = Image.open("welcome.jpg")
-        image = image.resize((600, 400), Image.ANTIALIAS)
-        image = ImageTk.PhotoImage(image)
-        image_label = tk.Label(self, image=image)
-        image_label.image = image
-        image_label.pack()
+        self.image = Image.open("welcome.jpg")
+        self.image = self.image.resize((600, 400), Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(self.image)
+        self.image_label = tk.Label(self, image=self.image)
+        self.image_label.image = self.image
+        self.image_label.pack()
 
         # Add a start button
-        start_button = tk.Button(self, text="START!", command=self.start_adventure)
-        start_button.pack(pady=10)
+        self.start_button = tk.Button(self, text="START!", command=self.start_adventure)
+        self.start_button.pack(pady=10)
 
     def start_adventure(self):
-        # This function will be called when the start button is clicked
+        # Remove the welcome label and start button
+        self.welcome_label.pack_forget()
+        self.start_button.pack_forget()
+
+        # Update the image
+        self.image = Image.open("park.jpg")
+        self.image = self.image.resize((600, 400), Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(self.image)
+        self.image_label.configure(image=self.image)
+        self.image_label.image = self.image
+
+        # Add the new text and buttons
+        self.text_label = tk.Label(self, text="On a cloudy day, you took your puppy to the park. You were playing fetch with your puppy, suddenly your puppy ran into the distance. You chased your puppy, puppy ran into a mysterious palace in the distance. You ran to the mysterious palace, You are standing in front of the door.", font=("Helvetica", 16), wraplength=550)
+        self.text_label.pack(side="top", fill="x", pady=10)
+        self.yes_button = tk.Button(self, text="Yes", command=self.enter_palace)
+        self.yes_button.pack(side="left", padx=10)
+        self.no_button = tk.Button(self, text="No", command=self.welcome_screen)
+        self.no_button.pack(side="left", padx=10)
+        self.quit_button = tk.Button(self, text="Quit", command=self.quit)
+        self.quit_button.pack(side="left", padx=10)
+
+    def enter_palace(self):
+        # This function will be called when the Yes button is clicked
         pass
+
+    def welcome_screen(self):
+        # Remove the text and buttons
+        self.text_label.pack_forget()
+        self.yes_button.pack_forget()
+        self.no_button.pack_forget()
+        self.quit_button.pack_forget()
+
+        # Show the welcome label and start button again
+        self.welcome_label.pack(side="top", fill="x", pady=10)
+        self.start_button.pack(pady=10)
 
 if __name__ == "__main__":
     game = AdventureGame()
