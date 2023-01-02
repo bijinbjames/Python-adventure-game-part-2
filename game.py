4d7d865c06d11023426fe7b0149d22a53db08342
bijinbjames <bijinbjames@gmail.com>
2023-01-02 20:37:41 +0530

    updated classes and objects

--- game.py
+++ game.py
@@ -1,146 +1,4 @@
 import tkinter as tk
-from PIL import Image, ImageTk
-
-
-class Game:
-    def __init__(self):
-        # Create the main window
-        self.root = tk.Tk()
-        self.root.title("Adventure Game")
-        
-        # Create the player and the rooms
-        self.player = Player()
-        self.entrance = Entrance()
-        self.gallery = Gallery()
-        self.arena = Arena()
-        self.throne_room = ThroneRoom()
-        self.playroom = Playroom()
-        self.chamber = Chamber()
-        self.arcade = Arcade()
-        self.scullery = Scullery()
-        self.dungeon = Dungeon()
-        self.undercroft = Undercroft()
-        
-        # Set the starting location for the player
-        self.player.location = self.entrance
-        
-        # Display the entrance
-        self.display_location()
-        
-    def display_location(self):
-        # Clear the window
-        for widget in self.root.winfo_children():
-            widget.destroy()
-        
-        # Display the image for the current location
-        image = Image.open(self.player.location.image)
-        photo = ImageTk.PhotoImage(image)
-        label = tk.Label(self.root, image=photo)
-        label.image = photo
-        label.pack()
-        
-        # Display the buttons for the available exits
-        for direction in self.player.location.exits:
-            button = tk.Button(self.root, text=direction, command=lambda d=direction: self.move(d))
-            button.pack()
-        
-        # Display the options for the current location
-        for option in self.player.location.options:
-            button = tk.Button(self.root, text=option, command=lambda o=option: self.do_option(o))
-            button.pack()
-    
-    def move(self, direction):
-        # Update the player's location
-        if direction == "North":
-            self.player.location = self.player.location.north
-        elif direction == "South":
-            self.player.location = self.player.location.south
-        elif direction == "East":
-            self.player.location = self.player.location.east
-        elif direction == "West":
-            self.player.location = self.player.location.west
-        
-        # Display the new location
-        self.display_location()
-    
-    def do_option(self, option):
-        # Handle the option selected by the player
-        if option == "Attack Witch":
-            if "Knife" in self.player.inventory:
-                # The player has the knife and can defeat the witch
-                self.player.location.options.remove("Attack Witch")
-                self.player.location.options.append("Take Golden Key")
-                self.display_location()
-            else:
-                # The player does not have the knife and dies
-                self.display_death()
-        elif option == "Take Knife":
-            self.player.inventory.append("Knife")
-            self.player.location.options.remove("Take Knife")
-        elif option == "Take Golden Key":
-            self.player.inventory.append("Golden Key")
-            self.player.location.options.remove("Take Golden Key")
-            self.display_location()
-        elif option == "Open Cell":
-            if "Golden Key" in self.player.inventory:
-                # The player has the golden key and can open the cell
-                self.player.location.options.remove("Open Cell")
-                self.player.location.options.append("Take Puppy")
-                self.display_location()
-            else:
-                # The player does not have the key and cannot open the cell
-                self.display_failure()
-        elif option == "Take Puppy":
-            self.display_victory()
-    
-    def display_death(self):
-        # Clear the window
-        for widget in self.root.winfo_children():
-            widget.destroy()
-        
-        # Display the death message
-        label = tk.Label(self.root, text="You have been killed by the witch. Game over.")
-        label.pack()
-        
-        # Display the play again button
-        button = tk.Button(self.root, text="Play Again", command=self.restart)
-        button.pack()
-    
-    def display_failure(self):
-        # Clear the window
-        for widget in self.root.winfo_children():
-            widget.destroy()
-        
-        # Display the failure message
-        label = tk.Label(self.root, text="You do not have the key to open the cell. Try again.")
-        label.pack()
-        
-        # Display the continue button
-        button = tk.Button(self.root, text="Continue", command=self.display_location)
-        button.pack()
-    
-    def display_victory(self):
-        # Clear the window
-        for widget in self.root.winfo_children():
-            widget.destroy()
-        
-        # Display the victory message
-        label = tk.Label(self.root, text="You have found the puppy! Congratulations, you have won the game!")
-        label.pack()
-        
-        # Display the play again button
-        button = tk.Button(self.root, text="Play Again", command=self.restart)
-        button.pack()
-    
-    def restart(self):
-        # Create a new game and start the main loop
-        game = Game()
-        game.root.mainloop()
-
-class Player:
-    def __init__(self):
-        self.location = None
-        self.inventory = []
 
 class Room:
     def __init__(self, name, image, north=None, south=None, east=None, west=None, options=[]):
@@ -151,62 +9,164 @@ class Room:
         self.east = east
         self.west = west
         self.options = options
+
 class Entrance(Room):
-    def __init__(self):
-        super().__init__(name="Entrance", image="entrance.jpg", north=Gallery())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Entrance", image="image/entrance.jpg", north=north, south=south, east=east, west=west)
 
 class Gallery(Room):
-    def __init__(self):
-        super().__init__(name="Gallery", image="gallery.jpg", south=Entrance(), options=["Attack Witch"])
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Gallery", image="image/gallery.jpg", north=north, south=south, east=east, west=west, options=["Attack Witch"])
 
 class Arena(Room):
-    def __init__(self):
-        super().__init__(name="Arena", image="arena.jpg", east=ThroneRoom())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Arena", image="image/arena.jpg", north=north, south=south, east=east, west=west)
 
 class ThroneRoom(Room):
-    def __init__(self):
-        super().__init__(name="Throne Room", image="throne_room.jpg", west=Arena(), south=Playroom())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Throne Room", image="image/throne_room.jpg", north=north, south=south, east=east, west=west)
 
 class Playroom(Room):
-    def __init__(self):
-        super().__init__(name="Playroom", image="playroom.jpg", north=ThroneRoom(), east=Chamber())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Playroom", image="image/playroom.jpeg", north=north, south=south, east=east, west=west)
 
 class Chamber(Room):
-    def __init__(self):
-        super().__init__(name="Chamber", image="chamber.jpg", west=Playroom(), north=Scullery())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Chamber", image="image/chamber.jpg", north=north, south=south, east=east, west=west)
 
 class Arcade(Room):
-    def __init__(self):
-        super().__init__(name="Arcade", image="arcade.jpg", north=Scullery(), south=Dungeon())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Arcade", image="image/arcade.jpg", north=north, south=south, east=east, west=west)
 
 class Scullery(Room):
-    def __init__(self):
-        super().__init__(name="Scullery", image="scullery.jpeg", south=Chamber(), options=["Take Knife"])
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Scullery", image="image/scullery.jpeg", north=north, south=south, east=east, west=west, options=["Take Knife"])
 
 class Dungeon(Room):
-    def __init__(self):
-        super().__init__(name="Dungeon", image="dungeon.jpg", north=Arcade(), south=Undercroft())
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Dungeon", image="image/dungeon.jpg", north=north, south=south, east=east, west=west)
 
 class Undercroft(Room):
-    def __init__(self):
-        super().__init__(name="Undercroft", image="undercroft.jpg", north=Dungeon(), options=["Open Cell"])
-        
-    @property
-    def exits(self):
-        # Return the available exits
-        exits = []
-        if self.north:
-            exits.append("North")
-        if self.south:
-            exits.append("South")
-        if self.east:
-            exits.append("East")
-        if self.west:
-            exits.append("West")
-        return exits
+    def __init__(self, north=None, south=None, east=None, west=None):
+        super().__init__(name="Undercroft", image="image/undercroft.jpeg", north=north, south=south, east=east, west=west, options=["Open Cell"])
 
+class Player:
+    def __init__(self):
+        self.inventory = []
+        self.current_room = None
+
+class Game:
+    def __init__(self):
+        # Create the Gallery and Entrance objects
+        arena = Arena()
+        throne_room = ThroneRoom()
+        arcade = Arcade()
+        playroom = Playroom()
+        gallery = Gallery(south=Entrance(), west=arena, east=throne_room)
+        entrance = Entrance(north=Gallery(), west=arcade, east=playroom)
+        chamber = Chamber(west=playroom)
+        scullery = Scullery(south=arcade)
+        undercroft = Undercroft(north=dungeon)
+        dungeon = Dungeon(north=arcade, south=undercroft)
+
+        # Set up the rest of the game
+        self.root = tk.Tk()
+        self.root.geometry("640x480")
+        self.player = Player()
+        self.current_room = entrance
+        self.player.current_room = self.current_room
+        self.image_label = tk.Label(self.root)
+        self.image_label.pack()
+        self.text_label = tk.Label(self.root, text=self.current_room.name)
+        self.text_label.pack()
+        self.button_frame = tk.Frame(self.root)
+        self.button_frame.pack()
+        self.update_display()
+
+    def update_display(self):
+        # Clear the button frame
+        for widget in self.button_frame.winfo_children():
+            widget.destroy()
+
+        # Update the text label
+        self.text_label.config(text=self.current_room.name)
+
+        # Update the image
+        image = tk.PhotoImage(file=self.current_room.image)
+        self.image_label.config(image=image)
+        self.image_label.image = image
+
+        # Create buttons for each option
+        for option in self.current_room.options:
+            button = tk.Button(self.button_frame, text=option, command=lambda opt=option: self.do_option(opt))
+            button.pack()
+
+        # Create buttons for each direction
+        if self.current_room.north:
+            button = tk.Button(self.button_frame, text="North", command=self.go_north)
+            button.pack()
+        if self.current_room.south:
+            button = tk.Button(self.button_frame, text="South", command=self.go_south)
+            button.pack()
+        if self.current_room.east:
+            button = tk.Button(self.button_frame, text="East", command=self.go_east)
+            button.pack()
+        if self.current_room.west:
+            button = tk.Button(self.button_frame, text="West", command=self.go_west)
+            button.pack()
+
+        def do_option(self, option):
+        # Handle the "Attack Witch" option
+            if option == "Attack Witch" and "Knife" in self.player.inventory:
+                self.current_room.options.remove("Attack Witch")
+                self.current_room.options.append("Take Golden Key")
+            elif option == "Attack Witch":
+                self.text_label.config(text="You were killed by the witch!")
+                self.button_frame.destroy()
+                self.image_label.destroy()
+
+        # Handle the "Take Knife" option
+            elif option == "Take Knife":
+                self.player.inventory.append("Knife")
+
+            # Handle the "Take Golden Key" option
+            elif option == "Take Golden Key":
+                self.player.inventory.append("Golden Key")
+
+            # Handle the "Open Cell" option
+            elif option == "Open Cell" and "Golden Key" in self.player.inventory:
+                self.text_label.config(text="You found your puppy and won the game!")
+                self.button_frame.destroy()
+                self.image_label.destroy()
+            elif option == "Open Cell":
+                self.text_label.config(text="You need the Golden Key to open the cell.")
+
+        # Update the display
+        self.update_display()
+
+    def go_north(self):
+        self.current_room = self.current_room.north
+        self.player.current_room = self.current_room
+        self.update_display()
+
+    def go_south(self):
+        self.current_room = self.current_room.south
+        self.player.current_room = self.current_room
+        self.update_display()
+
+    def go_east(self):
+        self.current_room = self.current_room.east
+        self.player.current_room = self.current_room
+        self.update_display()
+
+    def go_west(self):
+        self.current_room = self.current_room.west
+        self.player.current_room = self.current_room
+        self.update_display()
 
 # Create the game and start the main loop
 game = Game()
 game.root.mainloop()
+
+
 
---
+++ player.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self):
+        self.current_room = None
+        self.inventory = []
+        self.dead = False
